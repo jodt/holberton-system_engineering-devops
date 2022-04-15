d47f7fdb888b06a2bcd7fda309a6eb2ffeeaed26
jodt <joel.dumortier@gmail.com>
2022-04-15 09:23:21 +0000

    add all tasks

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,11 @@
+# this manifest create a file in /tmp
+
+file { 'school':
+    path    => '/tmp/school',
+    owner   => 'www-data',
+    group   =>'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
+}
+
+
