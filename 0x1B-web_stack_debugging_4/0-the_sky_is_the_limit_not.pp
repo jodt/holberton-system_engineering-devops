83d46fc61cda922c60dbcd383a1225a22ce9e3cb
jodt <joel.dumortier@gmail.com>
2022-06-09 09:14:47 +0000

    try bug fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 #bug fix requests on server
-exec { sed -i 's/-n 15/-n 4096/ /etc/default/nginx':
+exec { "sed -i 's/n 15/n 4096/' /etc/default/nginx":
     path => ['/bin'],
-};
-exec {'sudo service nginx restart':
+}
+exec {"sudo service nginx restart":
     path => ['/usr/bin'],
 }
